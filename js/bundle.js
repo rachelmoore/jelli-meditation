ed08c0a8cf2c1ce4a769ef51e001a7eb436143e5
Rachel Moore <rachel@rachelmoore.net>
2017-10-04 10:08:28 -0700

    Modify timer

--- js/bundle.js
+++ js/bundle.js
@@ -75,6 +75,19 @@ module.exports = __webpack_require__(1);
 /***/ (function(module, exports) {
 
 
+class Interval {
+
+
+
+    startInterval() {
+
+    }
+
+    stopInterval() {
+
+    }
+
+}
 
 /***/ })
 /******/ ]);
---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+class Interval {
+
+
+
+    startInterval() {
+
+    }
+
+    stopInterval() {
+
+    }
+
+}
