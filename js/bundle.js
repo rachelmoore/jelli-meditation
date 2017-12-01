c7da0b45b298d8167029a5b34dc89166ba9e9cba
Rachel Moore <rachel@rachelmoore.net>
2017-12-01 11:25:01 -0800

    Braches merged again

--- js/bundle.js
+++ js/bundle.js
@@ -247,7 +247,7 @@
 	
 	            setTimeout(function () {
 	                return _this.timer.start();
-	            }, animationInterval * 20);
+	            }, animationInterval * 30);
 	        }
 	    }, {
 	        key: "resetInterval",
